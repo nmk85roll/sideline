0597778125ebb46ae04b6f9ba292b367c380a4f7
Nicholas Klezek <nmk85roll@yahoo.com>
2026-08-16 12:03:28 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Sideline Chart — offline cache.
    Bump VERSION whenever index.html changes so iPads pick up the new build. */
-var VERSION = 'sideline-v1';
+var VERSION = 'sideline-v3';
 var SHELL = [
   './',
   './index.html',
